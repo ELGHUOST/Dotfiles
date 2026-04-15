d05dce79216ec94df735e1926e28016066dcc08f
Mohamed <157063368+ELGHUOST@users.noreply.github.com>
2026-04-15 10:29:14 +0200

    Remove NVIDIA graphics settings from graphismes.nix
    Removed NVIDIA graphics configuration from NixOS module.

--- nixos/modules/graphismes.nix
+++ nixos/modules/graphismes.nix
@@ -2,12 +2,4 @@
 {
   hardware.graphics.enable      = true;
   hardware.graphics.enable32Bit = true;
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    open               = true;
-    modesetting.enable = true;
-    nvidiaSettings     = true;
-  };
 }
